e01ac612c26497553b167d50503266008dfc8d65
Kristian Ekle <kristian.ekle@gmail.com>
2016-05-01 09:38:55 +0200

    initial

---
+++ python_code/Backprop_skeleton.py
@@ -0,0 +1,165 @@
+import math
+import random
+import copy
+
+
+# The transfer function of neurons, g(x)
+def logFunc(x):
+    return (1.0 / (1.0 + math.exp(-x)))
+
+
+# The derivative of the transfer function, g'(x)
+def logFuncDerivative(x):
+    return math.exp(-x) / (pow(math.exp(-x) + 1, 2))
+
+
+def randomFloat(low, high):
+    return random.random() * (high - low) + low
+
+
+# Initializes a matrix of all zeros
+def makeMatrix(I, J):
+    m = []
+    for i in range(I):
+        m.append([0] * J)
+    return m
+
+
+class NN:  # Neural Network
+    def __init__(self, numInputs, numHidden, learningRate=0.001):
+        # Inputs: number of input and hidden nodes. Assuming a single output node.
+        # +1 for bias node: A node with a constant input of 1. Used to shift the transfer function.
+        self.numInputs = numInputs + 1
+        self.numHidden = numHidden
+
+        # Current activation levels for nodes (in other words, the nodes' output value)
+        self.inputActivation = [1.0] * self.numInputs
+        self.hiddenActivations = [1.0] * self.numHidden
+        self.outputActivation = 1.0  # Assuming a single output.
+        self.learningRate = learningRate
+
+        # create weights
+        # A matrix with all weights from input layer to hidden layer
+        self.weightsInput = makeMatrix(self.numInputs, self.numHidden)
+        # A list with all weights from hidden layer to the single output neuron.
+        self.weightsOutput = [0 for i in range(self.numHidden)]  # Assuming single output
+        # set them to random vaules
+        for i in range(self.numInputs):
+            for j in range(self.numHidden):
+                self.weightsInput[i][j] = randomFloat(-0.5, 0.5)
+        for j in range(self.numHidden):
+            self.weightsOutput[j] = randomFloat(-0.5, 0.5)
+
+        # Data for the backpropagation step in RankNets.
+        # For storing the previous activation levels (output levels) of all neurons
+        self.prevInputActivations = []
+        self.prevHiddenActivations = []
+        self.prevOutputActivation = 0
+        # For storing the previous delta in the output and hidden layer
+        self.prevDeltaOutput = 0
+        self.prevDeltaHidden = [0 for i in range(self.numHidden)]
+        # For storing the current delta in the same layers
+        self.deltaOutput = 0
+        self.deltaHidden = [0 for i in range(self.numHidden)]
+
+    def propagate(self, inputs):
+        if len(inputs) != self.numInputs - 1:
+            raise ValueError('wrong number of inputs')
+
+        # input activations
+        self.prevInputActivations = copy.deepcopy(self.inputActivation)
+        for i in range(self.numInputs - 1):
+            self.inputActivation[i] = inputs[i]
+        self.inputActivation[-1] = 1  # Set bias node to -1.
+
+        # hidden activations
+        self.prevHiddenActivations = copy.deepcopy(self.hiddenActivations)
+        for j in range(self.numHidden):
+            sum = 0.0
+            for i in range(self.numInputs):
+                # print self.ai[i] ," * " , self.wi[i][j]
+                sum = sum + self.inputActivation[i] * self.weightsInput[i][j]
+            self.hiddenActivations[j] = logFunc(sum)
+
+        # output activations
+        self.prevOutputActivation = self.outputActivation
+        sum = 0.0
+        for j in range(self.numHidden):
+            sum = sum + self.hiddenActivations[j] * self.weightsOutput[j]
+        self.outputActivation = logFunc(sum)
+        return self.outputActivation
+
+    def computeOutputDelta(self):
+        p_ab = logFunc(self.prevOutputActivation - self.outputActivation)
+        self.prevDeltaOutput = logFuncDerivative(self.prevOutputActivation) * (1 - p_ab)
+        self.deltaOutput = logFuncDerivative(self.outputActivation) * (1 - p_ab)
+        # TODO: Implement the delta function for the output layer (see exercise text)
+
+    def computeHiddenDelta(self):
+        for h in range(self.numHidden):
+            self.prevDeltaHidden[h] = logFuncDerivative(self.prevHiddenActivations[h]) * self.weightsOutput[h] * (
+                self.prevDeltaOutput - self.deltaOutput)
+            self.deltaHidden[h] = logFuncDerivative(self.hiddenActivations[h]) * self.weightsOutput[h] * (
+                self.prevDeltaOutput - self.deltaOutput)
+            # TODO: Implement the delta function for the hidden layer (see exercise text)
+
+    def updateWeights(self):
+        # Update the weights of the network using the deltas (see exercise text)
+        for i in range(len(self.weightsInput)):
+            for j in range(len(self.weightsInput[i])):
+                self.weightsInput[i][j] += self.learningRate * (
+                    self.prevDeltaHidden[j] * self.prevInputActivations[i] - self.deltaHidden[j] * self.inputActivation[
+                        i])
+
+        for h in range(len(self.weightsOutput)):
+            self.weightsOutput[h] += self.learningRate * (
+                self.prevDeltaOutput * self.prevOutputActivation - self.deltaOutput * self.outputActivation)
+            # TODO: Update the weights of the network using the deltas (see exercise text)
+
+    def backpropagate(self):
+        self.computeOutputDelta()
+        self.computeHiddenDelta()
+        self.updateWeights()
+
+    # Prints the network weights
+    def weights(self):
+        print('Input weights:')
+        for i in range(self.numInputs):
+            print(self.weightsInput[i])
+        print()
+        print('Output weights:')
+        print(self.weightsOutput)
+
+    def train(self, patterns, iterations=1):
+
+        # To measure performance each iteration: Run for 1 iteration, then count misordered pairs.
+        for pair in patterns:
+            # -Propagate A
+            self.propagate(pair[0])
+            # -Propagate B
+            self.propagate(pair[1])
+            # -Backpropagate
+            self.backpropagate()
+        return self.countMisorderedPairs(patterns)
+
+    def countMisorderedPairs(self, patterns):
+        # Let the network classify all pairs of patterns. The highest output determines the winner.
+        correct = 0.0
+        miss = 0.0
+
+        for pair in patterns:
+
+            # Propagate A
+            a = self.propagate(pair[0])
+            # Propagate B
+            b = self.propagate(pair[1])
+
+            # if A>B: A wins. If B>A: B wins
+            if a > b:
+                correct += 1
+            else:
+                miss += 1
+
+        # Calculate the ratio of correct answers:
+        # errorRate = numMisses/(numRight+numMisses)
+        return miss / (correct + miss)
---
+++ python_code/dataLoaderSkeleton.py
@@ -0,0 +1,102 @@
+import Backprop_skeleton as Bp
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+
+
+# Class for holding your data - one object for each line in the dataset
+class dataInstance:
+    def __init__(self, qid, rating, features):
+        self.qid = qid  # ID of the query
+        self.rating = rating  # Rating of this site for this query
+        self.features = features  # The features of this query-site pair.
+
+    def __str__(self):
+        return "Datainstance - qid: " + str(self.qid) + ". rating: " + str(self.rating) + ". features: " + str(
+            self.features)
+
+
+# A class that holds all the data in one of our sets (the training set or the testset)
+class dataHolder:
+    def __init__(self, dataset):
+        self.dataset = self.loadData(dataset)
+
+    def loadData(self, file):
+        # Input: A file with the data.
+        # Output: A dict mapping each query ID to the relevant documents, like this: dataset[queryID] = [dataInstance1, dataInstance2, ...]
+        data = open(file)
+        dataset = {}
+        for line in data:
+            # Extracting all the useful info from the line of data
+            lineData = line.split()
+            rating = int(lineData[0])
+            qid = int(lineData[1].split(':')[1])
+            features = []
+            for elem in lineData[2:]:
+                if '#docid' in elem:  # We reached a comment. Line done.
+                    break
+                features.append(float(elem.split(':')[1]))
+            # Creating a new data instance, inserting in the dict.
+            di = dataInstance(qid, rating, features)
+            if qid in dataset.keys():
+                dataset[qid].append(di)
+            else:
+                dataset[qid] = [di]
+        return dataset
+
+
+def runRanker(trainingset, testset):
+    # Dataholders for training and testset
+    dhTraining = dataHolder(trainingset)
+    dhTesting = dataHolder(testset)
+
+    # Creating an ANN instance - feel free to experiment with the learning rate (the third parameter).
+    nn = Bp.NN(46, 10, 0.001)
+
+    trainingPatterns = []  # For holding all the training patterns we will feed the network
+    testPatterns = []  # For holding all the test patterns we will feed the network
+
+    for qid in dhTraining.dataset.keys():
+        # This iterates through every query ID in our training set
+        dataInstance = dhTraining.dataset[qid]  # All data instances (query, features, rating) for query qid
+
+        dataInstance.sort(key=lambda data: data.rating, reverse=True)
+
+        for current in dataInstance:
+            for other in dataInstance:
+                if not current is other:
+                    trainingPatterns.append([current.features, other.features])
+
+    for qid in dhTesting.dataset.keys():
+        # This iterates through every query ID in our test set
+        dataInstance = dhTesting.dataset[qid]
+
+        dataInstance.sort(key=lambda data: data.rating, reverse=True)
+
+        for current in dataInstance:
+            for other in dataInstance:
+                if current is not other:
+                    testPatterns.append([current.features, other.features])
+
+    # Check ANN performance before training
+    test_error = list()
+    test_error.append(nn.countMisorderedPairs(testPatterns))
+    training_error = list()
+    training_error.append(nn.countMisorderedPairs(trainingPatterns))
+
+    for i in range(25):
+        # Running 25 iterations, measuring testing performance after each round of training.
+        # Training
+        training_error.append(nn.train(trainingPatterns, iterations=1))
+
+        # Check ann
+        test_error.append(nn.countMisorderedPairs(testPatterns))
+
+        # Check ANN performance after training.
+        nn.countMisorderedPairs(testPatterns)
+
+    plt.plot(run, testError, 'r', run, trainingError, 'g')
+    plt.show()
+
+
+runRanker('datasets/train.txt', 'datasets/test.txt')
